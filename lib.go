9538d8ccb0f5cc4a6c98d0b08313b9eead5e8921
JanusDG <dmytro_bilusiak@epam.com>
2022-09-11 19:57:09 +0300

    'added documentatios'

--- lib.go
+++ lib.go
@@ -1,10 +1,12 @@
-package lib
+// Package libTimer provides simple timer with flags
+package libTimer
 
 import (
 	"fmt"
 	"time"
 )
 
+// Timer implemets timing and flagging
 type Timer struct {
 	startPoint time.Time
 	endPoint   time.Time
@@ -12,23 +14,28 @@ type Timer struct {
 	flagCount  int
 }
 
+// NewTimer returns a new Timer with initialized flag map
 func NewTimer() *Timer {
 	return &Timer{flags: make(map[int]time.Time)}
 }
 
+// StartTimer sets startPoint to current time
 func (t *Timer) StartTimer() {
 	t.startPoint = time.Now()
 }
 
+// EndTimer sets endPoint to current time
 func (t *Timer) EndTimer() {
 	t.endPoint = time.Now()
 }
 
+// FlagNow adds currents time to all flags
 func (t *Timer) FlagNow() {
 	t.flags[t.flagCount] = time.Now()
 	t.flagCount++
 }
 
+// Show prints the time stamps flags and overall timer time
 func (t Timer) Show() {
 	for k, v := range t.flags {
 		fmt.Println(k, v)
